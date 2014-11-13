0c835f9d6499ff8977583bc2ae78ae1db0480b76
trfd <baptiste.dupy@gmail.com>
2014-11-13 20:08:11 +0100

    Reticle

---
+++ Chromatism/Assets/ReticleState.cs
@@ -0,0 +1,48 @@
+﻿//
+// ReticleState.cs
+//
+// Author(s):
+//       Baptiste Dupy <baptiste.dupy@gmail.com>
+//
+// Copyright (c) 2014
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class ReticleState : MonoBehaviour
+{
+	private Animator m_animator;
+
+	void Start()
+	{
+		m_animator = GetComponent<Animator>();
+
+		GPEventManager.Instance.Register("EnemyTouched",OnEnemyHit);
+		GPEventManager.Instance.Register("EnemyDied",OnEnemyHit);
+
+	}
+
+	void OnEnemyHit(string evtName, GPEvent evt)
+	{
+		m_animator.SetTrigger("Hit");
+	}
+}
