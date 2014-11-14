8c61e3adb0ec92433eb32403b6975fe095c2575a
RomainVivier <romain.simon.vivier@gmail.com>
2014-11-14 22:10:01 +0100

    fin du menu

--- Chromatism/Assets/Scripts/Managers/GUI/Menu.cs
+++ Chromatism/Assets/Scripts/Managers/GUI/Menu.cs
@@ -10,21 +10,38 @@ public class Menu : MonoBehaviour {
 
 	void Start()
 	{
-		KeyBinder.Instance.DefineActions("SwitchUpMenu", new KeyActionConfig(KeyType.Menu, 0, SwitchMenu, null));
-		KeyBinder.Instance.DefineActions("SwitchDownMenu", new KeyActionConfig(KeyType.Menu, 0, SwitchMenu, null));
-		KeyBinder.Instance.DefineActions("EnterMenu", new KeyActionConfig(KeyType.Menu, 0, EnterMenu, null));
-		KeyBinder.Instance.DefineActions("Quit", new KeyActionConfig(KeyType.Menu, 0, () =>{ Application.Quit(); }, null));
+		Debug.Log("plop");
+//		KeyBinder.Instance.DefineActions("SwitchUpMenu", new KeyActionConfig(KeyType.Menu, 0, SwitchMenu, null));
+//		KeyBinder.Instance.DefineActions("SwitchDownMenu", new KeyActionConfig(KeyType.Menu, 0, SwitchMenu, null));
+//		KeyBinder.Instance.DefineActions("EnterMenu", new KeyActionConfig(KeyType.Menu, 0, EnterMenu, null));
+//		KeyBinder.Instance.DefineActions("Quit", new KeyActionConfig(KeyType.Menu, 0, () =>{ Application.Quit(); }, null));
 
 		Fabric.EventManager.Instance.PostEvent("music_menu_on");
 	}
 	
 	void Update()
 	{
-	
+		if(Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow))
+		{
+			SwitchMenu();
+		}
+
+		
+		if(Input.GetKeyUp(KeyCode.Return))
+		{
+			EnterMenu();
+		}
+
+		
+		if(Input.GetKeyUp(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
 	}
 
 	void SwitchMenu()
 	{
+		Debug.Log("switch");
 		_selector1.SetActive(!_selector1.activeSelf);
 		_selector2.SetActive(!_selector2.activeSelf); 
 	}
